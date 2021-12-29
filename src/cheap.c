7bdf410161f94c40a57feefd68d37f83bd59c638
Richard Schmitt <rschmitt@bluepeach.com>
2021-12-29 12:20:05 -0600

    reformat

--- src/cheap.c
+++ src/cheap.c
@@ -11,36 +11,30 @@
 
 #include "ofc/impl/heapimpl.h"
 
-OFC_VOID ofc_heap_init_impl (OFC_VOID)
-{
+OFC_VOID ofc_heap_init_impl(OFC_VOID) {
 }
 
-OFC_VOID ofc_heap_unload_impl (OFC_VOID)
-{
+OFC_VOID ofc_heap_unload_impl(OFC_VOID) {
 }
 
-OFC_LPVOID ofc_malloc_impl (OFC_SIZET size)
-{
-  OFC_LPVOID mem ;
+OFC_LPVOID ofc_malloc_impl(OFC_SIZET size) {
+    OFC_LPVOID mem;
 
-  mem = malloc (size) ;
-  return (mem) ;
+    mem = malloc(size);
+    return (mem);
 }
 
-OFC_VOID ofc_heap_check_alloc_impl (OFC_LPCVOID mem)
-{
+OFC_VOID ofc_heap_check_alloc_impl(OFC_LPCVOID mem) {
 }
 
-OFC_VOID ofc_free_impl (OFC_LPVOID mem)
-{
-  free (mem) ;
+OFC_VOID ofc_free_impl(OFC_LPVOID mem) {
+    free(mem);
 }
 
-OFC_LPVOID ofc_realloc_impl (OFC_LPVOID ptr, OFC_SIZET size)
-{
-  OFC_LPVOID mem ;
+OFC_LPVOID ofc_realloc_impl(OFC_LPVOID ptr, OFC_SIZET size) {
+    OFC_LPVOID mem;
 
-  mem = realloc (ptr, size) ;
-  return (mem) ;
+    mem = realloc(ptr, size);
+    return (mem);
 }
 
